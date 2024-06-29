488349c48f6a4e9f2c56f091ac2fe2a95fb8467c
SharaawyMohamed <sharawem585@gmail.com>
2024-06-29 09:55:45 +0300

    Add Contexts in EF

---
+++ GameZone/Data/Categories.cs
@@ -0,0 +1,6 @@
+﻿namespace GameZone.Data
+{
+    public class Categories
+    {
+    }
+}
--- GameZone/obj/Debug/net8.0/GameZone.AssemblyInfo.cs
+++ GameZone/obj/Debug/net8.0/GameZone.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameZone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c69377e61360c08947d5ca8d981b8ba0584c0bb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+945d3ba42fa83495e524869ac086c583aa0a5086")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameZone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameZone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
